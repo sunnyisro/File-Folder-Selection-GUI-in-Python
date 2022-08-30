c1fd933a5f42c25f045538cac8c8bb33e4b953f8
Sunny <105144544+sunnyisro@users.noreply.github.com>
2022-08-30 16:38:44 +0530

    Files added

---
+++ browse_gui.py
@@ -0,0 +1,29 @@
+"""
+Created on 30 Aug, 2022 at 11:24
+    Title: browse_gui.py - Browsing GUI for selecting Folders and Files
+    Description:
+        -   ...
+@author: Supantha Sen, nrsc, ISRO
+"""
+
+# Importing Modules
+from tkinter import filedialog
+
+# Importing Custom Modules
+...
+
+...
+
+def browse_folder():
+    directory = filedialog.askdirectory(initialdir = '/',
+                                        title = 'Select the Path Folder'
+                                       )
+    return directory
+
+
+def browse_file():
+    filename = filedialog.askopenfilename(initialdir = '/',
+                                          title = 'Select the File',
+                                          filetypes = (('all files','*.*'), ('text files', '*.txt*'))
+                                         )
+    return filename
---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+Created on 30 Aug, 2022 at 11:48
+    Title: main.py - ...
+    Description:
+        -   ...
+@author: Supantha Sen, nrsc, ISRO
+"""
+
+# Importing Modules
+...
+
+# Importing Custom Modules
+from browse_gui import *
+
+...
+
+folder_path = browse_folder()
+print(folder_path)
+
+file_path = browse_file()
+print(file_path)
